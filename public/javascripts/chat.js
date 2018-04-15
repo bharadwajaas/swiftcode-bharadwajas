706f10b38482796713746feefe9f0031c47e79d1
adityasg <sgaditya97@gmail.com>
2018-04-15 12:30:29 +0000

    changing the websocket url

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -9,7 +9,7 @@ app.controller('chatController', function ($scope, $sce) {
     $scope.messages = [];
     $scope.trust = $sce.trustAsHtml;
 
-    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+    var exampleSocket = new WebSocket('wss://swiftcode-chatter.herokuapp.com/chatSocket');
 
     exampleSocket.onmessage = function (event) {
         var jsonData = JSON.parse(event.data);
